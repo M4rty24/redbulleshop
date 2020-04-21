aa49a49fa30c10c68547c6068558a3cd5d160010
M4rty24 <>
2020-04-21 16:33:33 +0200

    done php

---
+++ index.php
@@ -0,0 +1,56 @@
+<?PHP
+echo "<h3>typy proměnných</h3>"
+$vypnuto = false;
+
+        echo "<br>"
+
+echo "$vypnuto";
+
+        echo "<br>"
+
+echo "-1776251";
+
+        echo "<br>"
+
+echo "1,444";
+
+        echo "<br>"
+
+print " TEXTOVÝ ŘETĚZEC";
+
+        echo "<br>"
+
+$umrel = "pošel";
+
+echo "Pepíček nám $umrel";
+
+        echo "<br>"
+
+echo "<img src="sro.png" width="512" height="240" alt title="REDBULL">";
+
+        echo "<br>"
+
+echo "<h3>zpětné lomítko</h3>"
+
+        echo "<br>"
+
+echo "\\";
+
+        echo "<br>"
+
+        $a = 5;
+        $b = 10;
+        var_dump($a+$b);
+        var_dump($a-$b);
+        var_dump($a/$b);
+        var_dump($a*$b);
+        var_dump($a++);
+        var_dump($a--);
+        var_dump($a+=$b);
+        var_dump($a-=$b);
+        var_dump($a/=$b);
+        var_dump($a==$b);
+        var_dump($a!=$b);
+        var_dump($a>$b);
+        var_dump($a<$b);
+?>
